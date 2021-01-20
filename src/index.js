499f463bae477d68fad8ae6a55ec2717661de374
kento.uoi <fishperson19@gmail.com>
2021-01-21 01:34:31 +0900

    first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Hello = (props) => {
+  const { name, age } = props
+  const bornYear = () => new Date().getFullYear() - age
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} year old</p>
+      <p> so you are probably born in {bornYear()}</p>
+    </div>
+  )
+}
+const Footer = () => {
+  return (
+    <div>
+      Greetings
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'peter'
+  const age = '10'
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name="george" age={26 + 10}/>
+      <Hello name={name} age={age} />
+      <Footer />
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
