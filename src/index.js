d50884291147d4731f610d4c11024fb5a2319178
kento.uoi <fishperson19@gmail.com>
2021-01-22 23:43:07 +0900

    condition rendering

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import React, { useState}from 'react';
 import ReactDOM from 'react-dom';
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        ボタンを押してみてよ
+      </div>
+    )
+  }
+  return (
+    <div>
+      あなたが押したのは: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
@@ -19,10 +40,10 @@ const App = () => {
   return (
     <div>
       {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
+      <Button onClick={handleLeftClick} text='left' />
+      <Button onClick={handleRightClick} text='right' />
       {right}
-      <p>{allClicks.join(' ')}</p>
+      <History allClicks={allClicks} />
     </div>
   )
 }
