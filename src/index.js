f41f83cedf015135cc9738657a8d4b0ca65f1280
kento.uoi <fishperson19@gmail.com>
2021-01-22 22:24:36 +0900

    handle-click

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = (props) => {
-  const { name, age } = props
+const Hello = ({ name, age }) => {
+  // const { name, age } = props
   const bornYear = () => new Date().getFullYear() - age
 
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} year old</p>
+      <p>Hello {name}, you are {age} year old</p>
       <p> so you are probably born in {bornYear()}</p>
     </div>
   )
@@ -20,17 +20,66 @@ const Footer = () => {
   )
 }
 
+const Display = ({counter}) => <div>{counter}</div>
+
+
+const Button = ({handleClick, text}) => (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+)
+
 const App = () => {
-  const name = 'peter'
-  const age = '10'
+  const [ counter, setCounter ] = useState(0)
+  
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
   return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name="george" age={26 + 10}/>
-      <Hello name={name} age={age} />
-      <Footer />
-    </>
+    <div>
+      <Display counter={counter} />
+      <Button handleClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        handleClick={setToZero}
+        text='zero'
+      />
+      <Button
+        handleClick={decreaseByOne}
+        text='minus'
+      />
+    </div>
   )
 }
+
+// let counter = 1
+
+// const refresh = () => {
+//   ReactDOM.render(<App counter={counter} />,
+//   document.getElementById('root'))
+// }
+
+// refresh()
+// counter += 1
+// refresh()
+// counter += 1
+// refresh()
+// setInterval(() => {
+//   refresh()
+//   counter += 1
+// }, 1000)
+  // const name = 'peter'
+  // const age = '10'
+  // return (
+  //   <>
+  //     <h1>Greetings</h1>
+  //     <Hello name="george" age={26 + 10}/>
+  //     <Hello name={name} age={age} />
+  //     <Footer />
+  //   </>
+  // )
+// }
 
 ReactDOM.render(<App />, document.getElementById('root'))
