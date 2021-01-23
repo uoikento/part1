2e47fbd32d0f90e61d209e773814b0b0552d41a7
kento.uoi <fishperson19@gmail.com>
2021-01-24 01:25:56 +0900

    correct

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ const App = ({ notes }) => {
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note =>
-          <Note key={note.id} note={note.content} />
+        {notes.map((note) =>
+          <Note key={note.id} note={note} />
         )}
       </ul>
     </div>
